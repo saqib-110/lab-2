86bab11c2da59ce572c708b29add54f1ac83242d
saqib-110 <saqibhassan901@gmail.com>
2024-11-18 18:57:26 +0000

    Update Refund.java

--- Lab3/Refund.java
+++ Lab3/Refund.java
@@ -3,13 +3,11 @@ public class Refund {
     private float amount;
     private String customerId;
 
-    // Constructor
     public Refund(float amount, String customerId) {
         this.amount = amount;
         this.customerId = customerId;
     }
 
-    // Method
     public void refundAmount() {
         System.out.println("Refunding amount: " + amount + " to customer ID: " + customerId);
     }
