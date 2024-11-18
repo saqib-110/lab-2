889e1fa2c2999ba9e1b33bf4515fa42c800a8cf7
saqib-110 <saqibhassan901@gmail.com>
2024-11-18 18:58:06 +0000

    Update BookingCounter.java

--- Lab3/BookingCounter.java
+++ Lab3/BookingCounter.java
@@ -1,6 +1,5 @@
 package Lab4;
 public class BookingCounter {
-    // Placeholder class to represent the booking counter in the system
     public void processBooking() {
         System.out.println("Booking Counter: Processing ticket booking.");
     }
